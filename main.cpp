8086e4aaeda2132f7b347b8927150fc3d2723f8c
CraftyRyte <craftyryte2011@gmail.com>
2025-06-19 16:42:45 +0530

    intial

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+    std::cout << "boy boy" << std::endl;
+    long long a = 0;
+    long long b = 1;
+    
+    long long c;
+    long long d = 45;
+
+    for (int i = 0; i < 25; i++){
+        c = a+b;
+        a = b;
+        b = c;
+
+        std::cout << c << std::endl;
+    }
+    std::cout << "Hello World!" << std::endl;    
+    return 0;
+}
+
